caaa876c206281796dcc7cf906d5b1588904cacc
Kamakshi Karakoti <kamakshikarakoti22@gmail.com>
2023-02-28 21:24:20 +0530

    Initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.scss'
+import { Link } from 'react-router-dom'
+
+export const Header = () => {
+    return (
+        <div class="header">
+            <Link to="/" class="header__logo">logo</Link>
+            <div class="header__nav">
+                <Link to="/about" class="nav__item">About</Link>
+                <Link to="/contact" class="nav__item">Contact</Link>
+            </div>
+        </div>
+    )
+}
