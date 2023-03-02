2382f3d6406668cb74d3644123fce6f506de48a5
Kamakshi Karakoti <kamakshikarakoti22@gmail.com>
2023-03-02 16:30:17 +0530

    update

--- src/pages/SubjectsAPI.js
+++ src/pages/SubjectsAPI.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 
 export const SubjectsAPI = async (searchTerm, offset, limit) => {
-    const url = `http://openlibrary.org/subjects/${searchTerm}.json?limit=${limit}&offset=${offset}`;
+    const url = `https://openlibrary.org/subjects/${searchTerm}.json?limit=${limit}&offset=${offset}`;
     console.log(url);
     const response = await fetch(url);
     const data = await response.json();
